67248314db58e85257661623c5e31844a518ac0d
Pratham Soni <rh0-209@accionlabs.in>
2019-06-27 23:20:58 +0530

    my second push in git

---
+++ src/DynamicGoogleSearch.java
@@ -0,0 +1,43 @@
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DynamicGoogleSearch {
+	
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "E:\\Drivers\\chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get("https://www.google.co.in");
+		
+		driver.findElement(By.xpath("//input[@role='combobox']")).sendKeys("testing");
+		
+		List<WebElement> list = driver.findElements(By.xpath("//ul[@role='listbox']//li//descendant::div[@class='sbl1']"));
+
+		System.out.println("Total no. of suggestions are : "+ list.size());
+		
+		for(int i=0; i<list.size(); i++)
+		{
+			System.out.println(list.get(i).getText());
+			if(list.get(i).getText().contains("testing life cycle")) {
+				list.get(i).click();
+				break;
+			}
+		}
+
+		driver.quit();
+	}
+
+}
